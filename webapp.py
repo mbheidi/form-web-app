53834c3bf0da34edbfe1afb58acb60a1904c83c1
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-03-18 16:28:21 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,5 +1,22 @@
-$ say 'hello world' 
+from flask import Flask, url_for, render_template, request
 
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
+@app.route("/")
+def render_main():
+    return render_template('home.html')
 
-
+@app.route("/response")
+def render_response():
+    question = request.args['Hi there! How are you?'] 
+    #The request object stores information about the request sent to the server.
+    #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
+    #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
+    if response == 'Good, thank you':
+        reply = "Great, How may I help you today?"
+    else:
+        reply = "I need help with my homework"
+    return render_template('response.html', response = reply)
+    
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
