3e7464f51b78ba0f2af4beffed5a20065f5a41bc
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-03-18 17:20:52 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,12 +8,12 @@ def render_main():
 
 @app.route("/response")
 def render_response():
-    question = request.args['Hi there! How are you?'] 
+    name = request.args['name'] 
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if response == 'Good, thank you':
-        reply = "Great, How may I help you today?"
+    if name == 'Heidi':
+        reply = "That's a cool name"
     else:
         reply = "I need help with my homework"
     return render_template('response.html', response = reply)
