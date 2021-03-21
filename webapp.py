f4a847bd2337c262575a90ac51904f8caf03c12e
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-03-21 01:58:28 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,6 +1,6 @@
 from flask import Flask, url_for, render_template, request
 
-app = Flask(webapp) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
 def render_main():
@@ -8,7 +8,7 @@ def render_main():
 
 @app.route("/response")
 def render_response():
-    choice = request.args['choice'] 
+     color = request.args['color'] 
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
@@ -18,5 +18,5 @@ def render_response():
         reply = "Try some of these! This link provides over 80 fun activities to do when you're bored!"
     return render_template('response.html', response = reply)
     
-if webapp=="main":
+if __name__=="__main__":
     app.run(debug=False, port=54321)
