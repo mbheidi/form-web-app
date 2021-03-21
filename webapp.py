4243e2de0b141b22cb458f3f2e1ef3f878d11cec
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-03-21 01:27:07 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,14 +8,14 @@ def render_main():
 
 @app.route("/response")
 def render_response():
-    name = request.args['name'] 
+    preference = request.args['preference'] 
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if name == 'Heidi':
-        reply = "That's a cool name"
+    if preference == 'Tell me a joke ':
+        reply = "Why was the math book sad? Because it had so many problems hahaha"
     else:
-        reply = "That's a pretty name"
+        reply = "Try some of these! This link provides over 80 fun activities to do when you're bored! https://www.goodhousekeeping.com/life/a26872864/what-to-do-when-bored/ "
     return render_template('response.html', response = reply)
     
 if __name__=="__main__":
